85ce786cb9b755f5a0bbfe63175e7ff6fa1955e3
schrecka <schrecka@umich.edu>
2020-12-03 18:15:50 -0500

    google analytics attempt

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,16 @@ module.exports = {
     // 'gatsby-plugin-offline',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "GA-TRACKING_ID", 'G-L9YG10626Y'// Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
   ],
 }
